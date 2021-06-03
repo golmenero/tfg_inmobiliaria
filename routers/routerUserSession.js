0427c442b7829fd450a2ff52ecc778b1e94e10fd
golmenero <charlygomezcolmenero@gmail.com>
2021-06-03 23:27:56 +0200

    :bug:

--- routers/routerUserSession.js
+++ routers/routerUserSession.js
@@ -1,5 +1,6 @@
 module.exports = function (app,express ) {
     let routerUserSession = express.Router();
+    let exclusiveRouterUserSession = express.Router();
 
     routerUserSession.use(function (req, res, next) {
         if(!req.session.user){
@@ -14,6 +15,19 @@ module.exports = function (app,express ) {
         }
     });
 
+    exclusiveRouterUserSession.use(function (req, res, next) {
+        if(!req.session.user){
+            req.flash('error', ["Debe iniciar sesión para poder acceder a esta página, lo sentimos."])
+            res.redirect("/login");
+        }
+        else if (!(req.session.user.permission == 'U')) {
+            req.flash('error', ["Debe ser un usuario registrado para poder acceder a ésta página, lo sentimos."])
+            res.redirect("/home");
+        } else {
+            next();
+        }
+    });
+
     app.use("/conversations", routerUserSession);
     app.use("/conversations/*", routerUserSession);
     app.use("/notifications/load", routerUserSession);
@@ -21,4 +35,6 @@ module.exports = function (app,express ) {
     app.use("/wishes/*", routerUserSession);
     app.use("/wishes", routerUserSession);
 
+    app.use("/users/edit", exclusiveRouterUserSession);
+    app.use("/users/delete", exclusiveRouterUserSession);
 };
